1e01c360abecfb9e56e4a48f004934a146f6d729
Rodrigo Zenteno <rodrigo.zenteno35@gmail.com>
2020-01-16 16:24:31 -0400

    cards

--- proyecto_tienda/src/components/HelloWorld.vue
+++ proyecto_tienda/src/components/HelloWorld.vue
@@ -13,7 +13,7 @@
           </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="carousel-control-prev-icon bg-dark" aria-hidden="true"></span>
           <span class="sr-only">Previous</span>
         </a>
         <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
