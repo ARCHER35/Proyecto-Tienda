ae0fc6df1f0e4dab0fad6040729d637806dceacf
Noelia Ustariz <noeliaustariz21@gmail.com>
2020-01-16 17:22:39 -0400

    Vistas

--- proyecto_tienda/src/router/index.js
+++ proyecto_tienda/src/router/index.js
@@ -17,6 +17,76 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/carne',
+    name: 'carne',
+    component: () => import(/**/ '@/views/Carne.vue')
+  },
+  {
+    path: '/pollo',
+    name: 'pollo',
+    component: () => import(/**/ '@/views/Pollo.vue')
+  },
+  {
+    path: '/huevo',
+    name: 'huevo',
+    component: () => import(/** */ '@/views/Huevo.vue')
+  },
+  {
+    path: '/arroz',
+    name: 'arroz',
+    component: () => import(/** */ '@/views/Arroz.vue')
+  },
+  {
+    path: '/azucar',
+    name: 'azucar',
+    component: () => import(/** */ '@/views/Azucar.vue')
+  },
+  {
+    path: '/fideo',
+    name: 'fideo',
+    component: () => import(/** */ '@/views/Fideo.vue')
+  },
+  {
+    path: '/sal',
+    name: 'sal',
+    component: () => import(/** */ '@/views/Sal.vue')
+  },
+  {
+    path: '/aceite',
+    name: 'aceite',
+    component: () => import(/** */ '@/views/Aceite.vue')
+  },
+  {
+    path: '/adereso',
+    name: 'adereso',
+    component: () => import(/**/ '@/views/Adereso.vue')
+  },
+  {
+    path: '/otro',
+    name: 'otro',
+    component: () => import(/**/ '@/views/Otro.vue')
+  },
+  {
+    path: '/embutido',
+    name: 'embutido',
+    component: () => import(/**/ '@/views/Embutido.vue')
+  },
+  {
+    path: '/condimento',
+    name: 'condimento',
+    component: () => import(/** */ '@/views/Condimento.vue')
+  },
+  {
+    path: '/lacteo',
+    name: 'lacteo',
+    component: () => import(/** */ '@/views/Lacteo.vue')
+  },
+  {
+    path: '/contacto',
+    name: 'contacto',
+    component: () => import(/** */ '@/views/Contacto.vue')
   }
 ]
 
---
+++ proyecto_tienda/src/views/Fideo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h1>Vista Fideo</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
