25b787f582fbab0fb9929c1f45dad5baad76b92e
Rodrigo Zenteno <rodrigo.zenteno35@gmail.com>
2020-01-17 17:04:45 -0400

    avance

--- proyecto_tienda/src/components/Footer.vue
+++ proyecto_tienda/src/components/Footer.vue
@@ -1,48 +1,59 @@
 <template>
-<footer class="bg-dark">
-  <div class="container-fluid text-center text-md-left">
-    <div class="row text-light" style="padding:15px">
-      <div class="col-md-6 mt-md-0 mt-3">
-        <img src="http://www.esferalibros.com/uploads/imagenes/libros/principal/201605/principal-como-explicarte-el-mundo-cris-es_med.jpg" height="100px" width="300px">
-        <p>Pedidos por Mayor y Menor Horarios de Atencion 6 am a 15pm</p>
-      </div>
-      <hr class="clearfix w-100 d-md-none pb-3">
-      <div class="col-2"> 
-        <h5 class="text-uppercase">Frial Cris</h5>
-        <ul class="list-unstyled">
-          <li>
-            <a href="#!" class="text-light">Direccion: Capitan Victor Ustaris Km 4 1/2</a>
-          </li>
-         
-        </ul>
-      </div>
-      <div class="col-md-3 mb-md-0 mb-3">
-        <h5 class="text-uppercase">Contactanos Por</h5>
-        <ul class="list-unstyled">
-          <li>
-            <img src="https://cdn.icon-icons.com/icons2/306/PNG/512/Facebook-Icon_34072.png" height="50px" alt="">
-            <a href="https://www.facebook.com/cristina.zenteno.1675?epa=SEARCH_BOX" class="text-light">Facebook</a>
-          </li>
-          <li>
-            <img src="https://cdn.icon-icons.com/icons2/306/PNG/512/Whatsapp-Icon_33936.png" height="50px" alt="">
-            <a class="text-light">72783477 <i class="fas fa-cart-arrow-down"></i> </a>
-          </li>
-        </ul>
+  <footer class="bg-dark">
+    <div class="container-fluid text-center text-md-left">
+      <div class="row text-light" style="padding:15px">
+        <div class="col-md-6 mt-md-0 mt-3">
+          <img src="https://scontent.fvvi1-2.fna.fbcdn.net/v/t1.15752-9/82224605_612882182613239_6994560468685684736_n.png?_nc_cat=102&_nc_oc=AQk7xVm6wA7uhWDnTkc2CZmP3sgG1ZW9gPkAB4DDq4NuE99Ss3EKN3vYG2lxBBxcfJA&_nc_ht=scontent.fvvi1-2.fna&oh=09c32db08f2f613bdd89f82c704c05b6&oe=5EA0D4A8" height="100px" width="300px"/>
+          <p>Pedidos por Mayor y Menor Horarios de Atencion 6 am a 15pm</p>
+        </div>
+        <hr class="clearfix w-100 d-md-none pb-3" />
+        <div class="col-2">
+          <h5 class="text-uppercase">Frial Cris</h5>
+          <ul class="list-unstyled">
+            <li>
+              <a href="#!" class="text-light">Direccion: Capitan Victor Ustaris Km 4 1/2</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-3 mb-md-0 mb-3">
+          <h5 class="text-uppercase">Contactanos Por</h5>
+          <ul class="list-unstyled">
+            <li>
+              <img
+                src="https://cdn.icon-icons.com/icons2/306/PNG/512/Facebook-Icon_34072.png"
+                height="50px"
+                alt
+              />
+              <a
+                href="https://www.facebook.com/cristina.zenteno.1675?epa=SEARCH_BOX"
+                class="text-light"
+              >Facebook</a>
+            </li>
+            <li>
+              <img
+                src="https://cdn.icon-icons.com/icons2/306/PNG/512/Whatsapp-Icon_33936.png"
+                height="50px"
+                alt
+              />
+              <a class="text-light">
+                72783477
+                <i class="fas fa-cart-arrow-down"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="footer-copyright text-center py-3 text-light">© 2020 Copyright:
-    <a href="https://mdbootstrap.com/education/bootstrap/"> Derechos Reservados</a>
-  </div>
-</footer>
+    <div class="footer-copyright text-center py-3 text-light">
+      © 2020 Copyright:
+      <a href="#">Derechos Reservados</a>
+    </div>
+  </footer>
 </template>
 
 <script>
-export default {
- 
-};
+export default {};
 </script>
 
 <style>
-
 </style>
